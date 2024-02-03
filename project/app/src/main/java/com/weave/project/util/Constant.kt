ce0c371c258570c537833ec6b0e5f1936971f7c6
moondev03 <moondev03@gmail.com>
2024-02-04 03:26:59 +0900

    :sparkles: UnSplash Access Key 수정

--- project/app/src/main/java/com/weave/project/util/Constant.kt
+++ project/app/src/main/java/com/weave/project/util/Constant.kt
@@ -4,5 +4,6 @@ import com.weave.project.BuildConfig
 
 object Constant {
     const val BASE_URL = "https://api.unsplash.com/"
-    const val ACCESS_KEY = BuildConfig.ACCESS_KEY
+//    const val ACCESS_KEY = BuildConfig.ACCESS_KEY
+    const val ACCESS_KEY = "edyEsCEAMgWR6BmntJytgl9bYOqCQBdTjL3WIAuz2U4"
 }
